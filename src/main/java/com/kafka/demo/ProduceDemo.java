0d219d31480dea2baec7739e57506f149a2260ae
pradeep <pradeep.ncst@gmail.com>
2019-03-10 14:53:22 -0400

    Kafkademo

---
+++ src/main/java/com/kafka/demo/ProduceDemo.java
@@ -0,0 +1,11 @@
+package com.kafka.demo;
+
+public class ProduceDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+
+        //create produce pro
+    }
+}
